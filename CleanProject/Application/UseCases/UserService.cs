3ce9403df8f1873ee2b404364f872c149f3682f5
Federica Senni <FEDERICA.SENNI@local.ittcesena.it>
2025-11-03 08:55:12 +0100

    implementazione classe UserService

---
+++ CleanProject/Application/UseCases/UserService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Dto;
+using Application.Interfaces;
+using Application.Mappers;
+using Domain.Model.Entities;
+
+
+namespace Application.UseCases
+{
+    public class UserService
+    {
+        private readonly IUser _repositoryUser;
+        public UserService(IUser repositoryUser)
+        {
+            _repositoryUser = repositoryUser;
+        }
+        public void Add(UserDto dto)
+        {
+            User person = dto.ToEntity();
+            _repositoryUser.AddUser(person);
+        }
+
+    }
+}
